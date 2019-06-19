271f02ed5565ef3b3a041e070bce91a0e8fba740
Sai samanth <sai12999.k@gmail.com>
2019-06-19 19:38:23 +0530

    msg

---
+++ src/main/java/com/herokudemo/servlets/Serv.java
@@ -0,0 +1,26 @@
+package com.herokudemo.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Serv
+ */
+public class Serv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+		request.setAttribute("result", Integer.parseInt(request.getParameter("input1"))+Integer.parseInt(request.getParameter("input2")));
+		rd.forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="Serv" method="post">
+<input type="text" name="input1">
+<input type="text" name="input2">
+<input type="submit">
+</form>
+</body>
+</html>
